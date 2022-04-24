3b3a16c2ef8299d1e6c77768d94e286ed06d2b3b
davorluc <d.lucic4@hotmail.com>
2022-04-24 15:59:40 +0200

    Added descriptive comments, added latency in $marco command, added on_message event, where bot reacts to someone sending damn

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ client = commands.Bot(command_prefix='$')
 
 
 @client.event
+# Terminal: Notification when bot connects to server/guild
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
 @client.event
+# Terminal: Notification when someone joins and  leaves server/guild
 async def on_member_join(member):
     print(f'{member} has joined.')
 
@@ -24,15 +26,30 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has left.')
 
+# The following block of code is temporary. All subject to change.
+
 
 @client.command()
+# Discord: Bot responds with "Polo!" and latency
 async def marco(ctx):
-    await ctx.send('Polo!')
+    await ctx.send(f'Polo! You just wasted {round(client.latency * 1000)} ms of my life. Congrats!')
 
 
 @client.command()
+# Discord: Bot dies
 async def die(ctx):
     await ctx.send('Fuck you')
     await ctx.send('https://tenor.com/bKM4f.gif')
+    # Maybe one day add feature where bot goes offline (literally dies)
+
+
+@client.event
+# Discord: Bot responds with Daniel and a gif when someone mentions damn
+async def on_message(message):
+    if message.content == 'damn':
+        channel = message.channel
+        await channel.send('Daniel')
+        await channel.send('https://tenor.com/bMaK0.gif')
+# temporary until here
 
 client.run(TOKEN)

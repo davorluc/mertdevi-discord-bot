1da679441ad8ceeb72be557c85bfa43053ce8d77
davorluc <d.lucic4@hotmail.com>
2022-06-06 13:05:43 +0200

    jojos bizarre adventure easter egg implementation done

--- cogs/eastereggs.py
+++ cogs/eastereggs.py
@@ -6,11 +6,25 @@ class Eastereggs(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+    
+    # If a user asks if this was a JoJo's reference, Chadbot confirm# If a user asks if this was a JoJo's reference, Chadbot confirms.
+    
+    @commands.Cog.listener("on_message")
+    async def jojo(self, message):
+        references = ["is this a jojo's reference?",
+                "Is this a JoJo's reference?",
+                "is this a jojo's reference?",
+                "Is this a JoJo's reference",
+                "is this a jojos reference",
+                "Is this a JoJos reference?",
+                "is this a jojos reference?",
+                "Is this a Jojos reference"]
+        if message.content in references:
+            channel = message.channel
+            await channel.send("https://c.tenor.com/vw7ogSgBWuYAAAAC/no-yes.gif")
+            await self.client.process_commands(message)
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send('This is a placeholder for this cog. Work in Progress')
-
+    #more eastereggs to come :)
 
 def setup(client):
     client.add_cog(Eastereggs(client))
